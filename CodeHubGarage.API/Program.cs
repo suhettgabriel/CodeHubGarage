5c624150711d7e2023e69bbe950a94c6c9903290
suhettgabriel <suhettgabriel1983@gmail.com>
2023-11-08 08:43:12 -0300

    Ajustes de dependencia e versão do .NET

--- CodeHubGarage.API/Program.cs
+++ CodeHubGarage.API/Program.cs
@@ -1,33 +1,11 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-// Register the Swagger generator
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CodeHubGarage API", Version = "v1" });
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    // Enable Swagger UI when running in development mode
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CodeHubGarage API v1");
-    });
-}
+        webBuilder.UseStartup<Startup>();
+    })
+    .Build();
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+host.Run();
--- CodeHubGarage.API/Startup.cs
+++ CodeHubGarage.API/Startup.cs
@@ -1,38 +1,35 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-// Register the Swagger generator
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+public class Startup
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CodeHubGarage API", Version = "v1" });
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    // Enable Swagger UI when running in development mode
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
+    public void ConfigureServices(IServiceCollection services)
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CodeHubGarage API v1");
-    });
+        services.AddControllers();
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "CodeHubGarage API", Version = "v1" });
+        });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseDeveloperExceptionPage();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "CodeHubGarage API v1");
+        });
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
